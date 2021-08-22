4a35c615bbb2064249eeb3016b675886360ee79f
Thomas Weber <weber_t@mailbox.org>
2021-08-22 17:54:18 -0500

    wry: Config support

--- wry/src/main.rs
+++ wry/src/main.rs
@@ -9,14 +9,33 @@ struct Config {
 }
 
 fn load_config() -> Config {
+  let config_data = std::fs::read("application_config.json").unwrap();
+  let json: serde_json::Value = serde_json::from_slice(&config_data).unwrap();
+
+  let icon_data: Vec<u8>;
+  let icon_width: u32;
+  let icon_height: u32;
+  match std::fs::read("application_icon.dat") {
+    Ok(data) => {
+      icon_data = data;
+      icon_width = json["iconWidth"].as_u64().unwrap() as u32;
+      icon_height = json["iconHeight"].as_u64().unwrap() as u32;
+    },
+    Err(..) => {
+      icon_data = vec![255, 0, 0, 255];
+      icon_width = 1;
+      icon_height = 1;
+    }
+  }
+
   Config {
-    title: "Test".to_string(),
-    index: "./www/index.html".to_string(),
-    width: 480,
-    height: 360,
-    icon_pixels: vec![255, 0, 0, 255],
-    icon_width: 1,
-    icon_height: 1
+    title: json["title"].as_str().unwrap().to_string(),
+    index: json["index"].as_str().unwrap().to_string(),
+    width: json["width"].as_i64().unwrap() as i32,
+    height: json["height"].as_i64().unwrap() as i32,
+    icon_pixels: icon_data,
+    icon_width,
+    icon_height
   }
 }
 
