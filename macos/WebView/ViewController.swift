97c20bafc159020f7b9d72399e4098007186c25f
Thomas Weber <weber_t@mailbox.org>
2021-08-22 21:39:00 -0500

    macos: Use black background before webview loads

--- macos/WebView/ViewController.swift
+++ macos/WebView/ViewController.swift
@@ -1,12 +1,19 @@
 import Cocoa
 import WebKit
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, WKNavigationDelegate {
     @IBOutlet var webView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView.navigationDelegate = self
         webView.configuration.preferences.setValue(true, forKey: "allowFileAccessFromFileURLs")
+        webView.isHidden = true
+        view.wantsLayer = true
+        view.layer?.backgroundColor = NSColor.black.cgColor
         let url = Bundle.main.url(forResource: "index", withExtension: "html")!
         webView.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
+    }
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        webView.isHidden = false
     }
 }
